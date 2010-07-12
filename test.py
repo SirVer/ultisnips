b437f30fc7eb6a330969e5ccf9c4b621e578d810
Holger Rapp <SirVer@gmx.de>
2010-07-12 14:52:12 +0200

    Applied patch from Chris Chambers

--- test.py
+++ test.py
@@ -1059,6 +1059,24 @@ class Completion_SimpleExample_ECR(_VimTest):
     wanted = "superkallifragilistik\nsuperkallifragilistik some more " \
             "superkallifragilistik some more"
 
+# We need >2 different words with identical starts to create the
+# popup-menu:
+COMPLETION_OPTIONS = "completion1\ncompletion2\n"
+
+class Completion_ForwardsJumpWithoutCOMPL_ACCEPT(_VimTest):
+    # completions should not be truncated when JF is activated without having
+    # pressed COMPL_ACCEPT (Bug #598903)
+    snippets = ("test", "$1 $2")
+    keys = COMPLETION_OPTIONS + "test" + EX + "com" + COMPL_KW + JF + "foo"
+    wanted = COMPLETION_OPTIONS + "completion1 foo"
+
+class Completion_BackwardsJumpWithoutCOMPL_ACCEPT(_VimTest):
+    # completions should not be truncated when JB is activated without having
+    # pressed COMPL_ACCEPT (Bug #598903)
+    snippets = ("test", "$1 $2")
+    keys = COMPLETION_OPTIONS + "test" + EX + "foo" + JF + "com" + COMPL_KW + \
+           JB + "foo"
+    wanted = COMPLETION_OPTIONS + "foo completion1"
 
 ###################
 # SNIPPET OPTIONS #
