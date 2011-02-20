9eb0e5b810a73e20c361f782fea1a569bf66e99e
Holger Rapp <SirVer@gmx.de>
2011-02-20 21:22:37 +0100

    Parsing ultisnips takes even longer

--- test.py
+++ test.py
@@ -2300,7 +2300,7 @@ if __name__ == '__main__':
 
     # Now, source our runtime
     send(":so plugin/UltiSnips.vim\n", options.session)
-    time.sleep(.4) # Parsing and intializing UltiSnips takes a while.
+    time.sleep(2) # Parsing and initializing UltiSnips takes a while.
 
     # Inform all test case which screen session to use
     suite = unittest.TestSuite()
