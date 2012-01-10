324681d8cce40e20e5afdc3d44ef723a8202b421
Holger Rapp <HolgerRapp@gmx.net>
2012-01-10 20:11:52 +0100

    Support for unicode in Langmap. This fixes bug 871357

--- test.py
+++ test.py
@@ -2357,6 +2357,27 @@ hi4"""
     def _options_off(self):
         self.send(":set langmap=\n")
 
+#######################
+# Test for bug 871357 #
+#######################
+class TestLangmapWithUtf8_ExceptCorrectResult(_VimTest):
+    snippets = ("testme",
+"""my snipped ${1:some_default}
+and a mirror: $1
+$2...$3
+$0""")
+    keys = "testme" + EX + "hi1" + JF + "hi2" + JF + "hi3" + JF + "hi4"
+    wanted ="""my snipped hi1
+and a mirror: hi1
+hi2...hi3
+hi4"""
+
+    def _options_on(self):
+        self.send(":set langmap=йq,цw,уe,кr,еt,нy,гu,шi,щo,зp,х[,ъ],фa,ыs,вd,аf,пg,рh,оj,лk,дl,ж\\;,э',яz,чx,сc,мv,иb,тn,ьm,ю.,ё',ЙQ,ЦW,УE,КR,ЕT,НY,ГU,ШI,ЩO,ЗP,Х\{,Ъ\},ФA,ЫS,ВD,АF,ПG,РH,ОJ,ЛK,ДL,Ж\:,Э\",ЯZ,ЧX,СC,МV,ИB,ТN,ЬM,Б\<,Ю\>\n")
+
+    def _options_off(self):
+        self.send(":set langmap=\n")
+
 ########################
 # Tests for bug 616315 #
 ########################
