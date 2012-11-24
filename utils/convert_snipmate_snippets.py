b6c134e5845945b9afcc732407ce618b9234cd73
Steve McKinney <steve.mckinney@gmail.com>
2012-11-24 00:41:31 -0500

    Fixes conversion if file contains comments
    Snippets were converted incorrectly if file contained comments.
    Now snippets are converted correctly and comments are preserved.

--- utils/convert_snipmate_snippets.py
+++ utils/convert_snipmate_snippets.py
@@ -22,9 +22,9 @@ def convert_snippet_file(source):
         # Ignore empty lines
         if line.strip() == "":
             continue
-        # The rest of the handlig is stateful
+        # The rest of the handling is stateful
         if state == 0:
-            # Find snippet start
+            # Find snippet start. Keep comments.
             if line[:8] == "snippet ":
                 snippet_info = re.match("(\S+)\s*(.*)", line[8:])
                 if not snippet_info:
@@ -33,6 +33,9 @@ def convert_snippet_file(source):
                 retval += 'snippet %s "%s"' % (snippet_info.group(1), snippet_info.group(2) if snippet_info.group(2) else snippet_info.group(1)) + "\n"
                 state = 1
                 snippet = ""
+            elif line[:1] == "#":
+                retval += line
+                state = 0
         elif state == 1:
             # First line of snippet: Get indentation
             whitespace = re.search("^\s+", line)
@@ -45,10 +48,12 @@ def convert_snippet_file(source):
                 snippet += line[len(whitespace):]
                 state = 2
         elif state == 2:
-            # In snippet: Check if indentation level is the same. If not, snippet has ended
-            if line[:len(whitespace)] != whitespace:
+            # In snippet: If indentation level is the same, add to snippet. Else end snippet.
+            if line[:len(whitespace)] == whitespace:
+                snippet += line[len(whitespace):]
+            else:
                 retval += convert_snippet_contents(snippet) + "endsnippet\n\n"
-                # Copy-paste the section from state=0 so that we don't skip every other snippet
+                #Copy-paste the section from state=0 so that we don't skip every other snippet
                 if line[:8] == "snippet ":
                     snippet_info = re.match("(\S+)\s*(.*)", line[8:])
                     if not snippet_info:
@@ -57,8 +62,9 @@ def convert_snippet_file(source):
                     retval += 'snippet %s "%s"' % (snippet_info.group(1), snippet_info.group(2) if snippet_info.group(2) else snippet_info.group(1)) + "\n"
                     state = 1
                     snippet = ""
-            else:
-                snippet += line[len(whitespace):]
+                elif line[:1] == "#":
+                    retval += line
+                    state = 0
     if state == 2:
         retval += convert_snippet_contents(snippet) + "endsnippet\n\n"
     return retval
