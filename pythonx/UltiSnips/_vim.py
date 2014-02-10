d0e7fa747f0e939590838ccca5f191bb89841308
Holger Rapp <HolgerRapp@gmx.net>
2014-02-10 08:54:28 +0100

    Do not set a filetype in scratch buffers.

--- pythonx/UltiSnips/_vim.py
+++ pythonx/UltiSnips/_vim.py
@@ -101,7 +101,7 @@ def feedkeys(keys, mode='n'):
 def new_scratch_buffer(text):
     """Create a new scratch buffer with the text given"""
     command("botright new")
-    command("set ft=text")
+    command("set ft=")
     command("set buftype=nofile")
 
     vim.current.buffer[:] = text.splitlines()
