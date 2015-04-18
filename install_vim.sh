3ccc14dcac23432d0942ff7d0e18acdedafbdaa4
Holger Rapp <HolgerRapp@gmx.net>
2015-04-18 18:16:08 +0200

    Try to work around transiently failing commands.

--- install_vim.sh
+++ install_vim.sh
@@ -40,13 +40,23 @@ build_vanilla_vim () {
    rm -rf vim_build
 }
 
+repeat_transiently_failing_command () {
+   COMMAND=$1; shift
+
+   set +e
+   until ${COMMAND}; do
+      sleep 10
+   done
+   set -e
+}
+
 # Clone the dependent plugins we want to use.
 ./test_all.py --clone-plugins
 
 # Install tmux (> 1.8) and vim. 
-add-apt-repository ppa:kalakris/tmux -y
-apt-get update -qq
-apt-get install -qq -y tmux 
+repeat_transiently_failing_command "add-apt-repository ppa:kalakris/tmux -y"
+repeat_transiently_failing_command "apt-get update -qq"
+repeat_transiently_failing_command "apt-get install -qq -y tmux"
 
 if [[ $VIM_VERSION == "74" ]]; then
    build_vanilla_vim ftp://ftp.vim.org/pub/vim/unix/vim-7.4.tar.bz2
