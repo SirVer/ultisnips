0b57240bc3f4069066321bbac422b9c6efd44445
Holger Rapp <HolgerRapp@gmx.net>
2015-04-18 14:50:15 +0200

    x

--- travis_install.sh
+++ travis_install.sh
@@ -31,9 +31,9 @@ apt-get update -qq
 apt-get install -qq -y tmux 
 
 
-if [[ $VIM_VERSION = "73" ]]; then
+if [[ "$VIM_VERSION" = "73" ]]; then
    build_vim ftp://ftp.vim.org/pub/vim/unix/vim-7.3.tar.bz2
-elif [[ $VIM_VERSION = "74" ]]; then
+elif [[ "$VIM_VERSION" = "74" ]]; then
    build_vim ftp://ftp.vim.org/pub/vim/unix/vim-7.4.tar.bz2
 fi
 
