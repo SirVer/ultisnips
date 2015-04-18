e5b0911f503d8b0f377b23dc75d9ba3f71054b68
Holger Rapp <HolgerRapp@gmx.net>
2015-04-18 14:34:03 +0200

    x

--- travis_install.sh
+++ travis_install.sh
@@ -3,7 +3,29 @@
 # Installs dependencies on travis and prepares the test run.
 set -ex
 
-env
+build_vim () {
+   URL=$1; shift; 
+   curl $URL -o /tmp/vim.tar.bz2
+   tar xjvf /tmp/vim.tar.bz2 -C /tmp
+   cd /tmp/vim${VIM_VERSION}
+
+   ./configure \
+      --prefix=${HOME} \
+      --disable-nls \
+      --enable-gui=no \
+      --enable-multibyte \
+      --enable-pythoninterp \
+      --with-features=huge \
+      --with-tlib=ncurses \
+      --without-x
+   make install
+}
+
+if [[ $VIM_VERSION = "73" ]]; then
+   build_vim ftp://ftp.vim.org/pub/vim/unix/vim-7.3.tar.bz2
+elif [[ $VIM_VERSION = "74" ]]; then
+   build_vim ftp://ftp.vim.org/pub/vim/unix/vim-7.4.tar.bz2
+fi
 
 # Install tmux (> 1.8) and vim. 
 add-apt-repository ppa:kalakris/tmux -y
