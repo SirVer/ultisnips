121e25a7ef027fc317abcce897a40cd728c05c0f
Holger Rapp <HolgerRapp@gmx.net>
2015-04-18 14:45:37 +0200

    x

--- travis_install.sh
+++ travis_install.sh
@@ -8,8 +8,8 @@ VIM_VERSION=$1; shift
 build_vim () {
    URL=$1; shift; 
    curl $URL -o /tmp/vim.tar.bz2
-   tar xjvf /tmp/vim.tar.bz2 -C /tmp
-   cd /tmp/vim${VIM_VERSION}
+   tar xjf /tmp/vim.tar.bz2 -C /tmp
+   pushd /tmp/vim${VIM_VERSION}
 
    ./configure \
       --prefix=${HOME} \
@@ -21,7 +21,15 @@ build_vim () {
       --with-tlib=ncurses \
       --without-x
    make install
+   
+   popd
 }
+
+# Install tmux (> 1.8) and vim. 
+add-apt-repository ppa:kalakris/tmux -y
+apt-get update -qq
+apt-get install -qq -y tmux 
+
 
 if [[ $VIM_VERSION = "73" ]]; then
    build_vim ftp://ftp.vim.org/pub/vim/unix/vim-7.3.tar.bz2
@@ -29,10 +37,7 @@ elif [[ $VIM_VERSION = "74" ]]; then
    build_vim ftp://ftp.vim.org/pub/vim/unix/vim-7.4.tar.bz2
 fi
 
-# Install tmux (> 1.8) and vim. 
-add-apt-repository ppa:kalakris/tmux -y
-apt-get update -qq
-apt-get install -qq -y tmux 
+which vim
 
 # Clone the dependent plugins we want to use.
 ./test_all.py --clone-plugins
