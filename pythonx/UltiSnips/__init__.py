559c90bfd68f5e3073f34793697ea3ddf22646f4
Stanislav Seletskiy <s.seletskiy@gmail.com>
2015-06-24 15:05:48 +0600

    fix python3, finally

--- pythonx/UltiSnips/__init__.py
+++ pythonx/UltiSnips/__init__.py
@@ -2,12 +2,3 @@
 # encoding: utf-8
 
 """Entry point for all thinks UltiSnips."""
-
-import vim  # pylint:disable=import-error
-
-from UltiSnips.snippet_manager import SnippetManager
-
-UltiSnips_Manager = SnippetManager(  # pylint:disable=invalid-name
-    vim.eval('g:UltiSnipsExpandTrigger'),
-    vim.eval('g:UltiSnipsJumpForwardTrigger'),
-    vim.eval('g:UltiSnipsJumpBackwardTrigger'))
