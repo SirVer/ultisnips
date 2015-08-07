dc254bec04c39eb6c2d7cfeb97dcef6458fd2308
Greg Hurrell <greg@hurrell.net>
2015-08-06 23:25:03 -0700

    Only use <nomodeline> if we're sure it is available
    According to `:h version7.txt`, `<nomodeline>` was added in Vim 7.3.438.
    Trying to use it prior to that will likely result in an:
    
        E216: No such group or event: <nomodeline> User UltiSnipsEnterFirstSnippet
    
    Add a conditional to ensure we only use it when we know it's available.
    
    Follow-up to: https://github.com/SirVer/ultisnips/pull/544

--- pythonx/UltiSnips/snippet_manager.py
+++ pythonx/UltiSnips/snippet_manager.py
@@ -374,7 +374,10 @@ class SnippetManager(object):
             'autocmd CmdwinLeave * call UltiSnips#LeavingBuffer()')
         _vim.command('augroup END')
 
-        _vim.command('silent doautocmd <nomodeline> User UltiSnipsEnterFirstSnippet')
+        if _vim.eval('v:version > 703 || v:version == 703 && has("patch438")') == '1':
+            _vim.command('silent doautocmd <nomodeline> User UltiSnipsEnterFirstSnippet')
+        else:
+            _vim.command('silent doautocmd User UltiSnipsEnterFirstSnippet')
         self._inner_state_up = True
 
     def _teardown_inner_state(self):
@@ -382,7 +385,10 @@ class SnippetManager(object):
         if not self._inner_state_up:
             return
         try:
-            _vim.command('silent doautocmd <nomodeline> User UltiSnipsExitLastSnippet')
+            if _vim.eval('v:version > 703 || v:version == 703 && has("patch438")') == '1':
+                _vim.command('silent doautocmd <nomodeline> User UltiSnipsExitLastSnippet')
+            else:
+                _vim.command('silent doautocmd User UltiSnipsExitLastSnippet')
             if self.expand_trigger != self.forward_trigger:
                 _vim.command('iunmap <buffer> %s' % self.forward_trigger)
                 _vim.command('sunmap <buffer> %s' % self.forward_trigger)
