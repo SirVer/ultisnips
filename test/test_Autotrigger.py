8ff84758a7b097b0da56cdccd7195d1670810f1f
Stanislav Seletskiy <s.seletskiy@gmail.com>
2015-08-12 17:06:56 +0600

    fix tests
    Autotrigger is not supported for old versions of vim.

--- test/test_Autotrigger.py
+++ test/test_Autotrigger.py
@@ -1,8 +1,22 @@
 from test.vim_test_case import VimTestCase as _VimTest
 from test.constant import *
 
+import subprocess
+
+
+def has_patch(version, executable):
+    output = subprocess.check_output([executable, "--version"])
+    for line in output.split("\n"):
+        if line.startswith("Included patches:"):
+            patch = line.split('-')[1]
+
+    return int(patch) >= version
+
 
 class Autotrigger_CanMatchSimpleTrigger(_VimTest):
+    skip_if = lambda self: 'Vim newer than 7.4.214 is required' if \
+        not has_patch(214, self.vim._vim_executable) \
+        else None
     files = { 'us/all.snippets': r"""
         snippet a "desc" A
         autotriggered
@@ -13,6 +27,9 @@ class Autotrigger_CanMatchSimpleTrigger(_VimTest):
 
 
 class Autotrigger_CanMatchContext(_VimTest):
+    skip_if = lambda self: 'Vim newer than 7.4.214 is required' if \
+        not has_patch(214, self.vim._vim_executable) \
+        else None
     files = { 'us/all.snippets': r"""
         snippet a "desc" "snip.line == 2" Ae
         autotriggered
