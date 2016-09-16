9c39aa1bbd0e94e7531b8e00cd3ea8ad5922f0f6
Marius Gedminas <marius@gedmin.as>
2016-09-16 23:25:15 +0300

    Only use vim.bindeval() if it exists

--- pythonx/UltiSnips/_vim.py
+++ pythonx/UltiSnips/_vim.py
@@ -250,7 +250,13 @@ def _unmap_select_mode_mapping():
                     '| redir END')
 
             # Check if any mappings where found
-            all_maps = list(filter(len, bindeval(r"_tmp_smaps").splitlines()))
+            if hasattr(vim, 'bindeval'):
+                # Safer to use bindeval, if it exists, because it can deal with
+                # non-UTF-8 characters in mappings; see GH #690.
+                all_maps = bindeval(r"_tmp_smaps")
+            else:
+                all_maps = eval(r"_tmp_smaps")
+            all_maps = list(filter(len, all_maps.splitlines()))
             if len(all_maps) == 1 and all_maps[0][0] not in ' sv':
                 # "No maps found". String could be localized. Hopefully
                 # it doesn't start with any of these letters in any
