3337d58e7f25be6d71492e02a0b342035ae62878
Holger Rapp <HolgerRapp@gmx.net>
2018-04-01 00:28:27 +0200

    FTP does not work :(.

--- scripts/download_vim.sh
+++ scripts/download_vim.sh
@@ -4,6 +4,6 @@ set -o errexit
 set -o verbose
 
 mkdir -p /src && cd /src
-curl ftp://ftp.vim.org/pub/vim/unix/vim-${VIM_VERSION}.tar.bz2 -o vim.tar.bz2
+curl http://ftp.vim.org/pub/vim/unix/vim-${VIM_VERSION}.tar.bz2 -o vim.tar.bz2
 tar xjf vim.tar.bz2
 mv -v vim?? vim
