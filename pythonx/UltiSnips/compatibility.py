f51f867c363d4ad961103d49fb3b406812a97c6b
Holger Rapp <HolgerRapp@gmx.net>
2019-11-05 10:30:30 +0100

    x

--- pythonx/UltiSnips/compatibility.py
+++ pythonx/UltiSnips/compatibility.py
@@ -6,12 +6,14 @@ versions as possible."""
 
 import vim
 
+
 def _vim_dec(string):
     """Decode 'string' using &encoding."""
     # We don't have the luxury here of failing, everything
     # falls apart if we don't return a bytearray from the
     # passed in string
     return string.decode(vim.eval("&encoding"), "replace")
+
 
 def _vim_enc(bytearray):
     """Encode 'string' using &encoding."""
@@ -20,6 +22,7 @@ def _vim_enc(bytearray):
     # in bytearray
     return bytearray.encode(vim.eval("&encoding"), "replace")
 
+
 def col2byte(line, col):
     """Convert a valid column index into a byte index inside of vims
     buffer."""
@@ -27,12 +30,14 @@ def col2byte(line, col):
     pre_chars = (vim.current.buffer[line - 1] + "  ")[:col]
     return len(_vim_enc(pre_chars))
 
+
 def byte2col(line, nbyte):
     """Convert a column into a byteidx suitable for a mark or cursor
     position inside of vim."""
     line = vim.current.buffer[line - 1]
     raw_bytes = _vim_enc(line)[:nbyte]
     return len(_vim_dec(raw_bytes))
+
 
 def as_unicode(string):
     """Return 'string' as unicode instance."""
