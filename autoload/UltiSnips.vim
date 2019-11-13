SirVer/ultisnips

commit d2f42d6b43902e5b2ef7bfca2579ccb6cc9f52c0

Remove support for Python2 (#1129)
Python 3 is > 10 years old and Python 2 will no longer be supported in a few months. Time to get rid of it, simplifying the code base (slightly) and opening the opportunities to use the Py3 quality of life improvements.

autoload/UltiSnips.vim
@@ -1,11 +1,11 @@
-if exists("b:did_autoload_ultisnips") || !exists("g:_uspy")
+if exists("b:did_autoload_ultisnips")
     finish
 endif
 let b:did_autoload_ultisnips = 1
 
 " Also import vim as we expect it to be imported in many places.
-exec g:_uspy "import vim"
-exec g:_uspy "from UltiSnips import UltiSnips_Manager"
+py3 import vim
+py3 from UltiSnips import UltiSnips_Manager
 
 function! s:compensate_for_pum() abort
     """ The CursorMovedI event is not triggered while the popup-menu is visible,
@@ -13,7 +13,7 @@ function! s:compensate_for_pum() abort
     """ to explicitly check for the presence of the popup menu, and update
     """ the vim-state accordingly.
     if pumvisible()
-        exec g:_uspy "UltiSnips_Manager._cursor_moved()"
+        py3 UltiSnips_Manager._cursor_moved()
     endif
 endfunction
 
@@ -23,7 +23,7 @@ function! UltiSnips#Edit(bang, ...) abort
     else
         let type = ""
     endif
-    exec g:_uspy "vim.command(\"let file = '%s'\" % UltiSnips_Manager._file_to_edit(vim.eval(\"type\"), vim.eval('a:bang')))"
+    py3 vim.command("let file = '%s'" % UltiSnips_Manager._file_to_edit(vim.eval("type"), vim.eval('a:bang')))
 
     if !len(file)
        return
@@ -48,7 +48,7 @@ function! UltiSnips#Edit(bang, ...) abort
 endfunction
 
 function! UltiSnips#AddFiletypes(filetypes) abort
-    exec g:_uspy "UltiSnips_Manager.add_buffer_filetypes('" . a:filetypes . "')"
+    py3 UltiSnips_Manager.add_buffer_filetypes('" . a:filetypes . "')
     return ""
 endfunction
 
@@ -69,18 +69,18 @@ function! UltiSnips#FileTypeComplete(arglead, cmdline, cursorpos) abort
 endfunction
 
 function! UltiSnips#ExpandSnippet() abort
-    exec g:_uspy "UltiSnips_Manager.expand()"
+    py3 UltiSnips_Manager.expand()
     return ""
 endfunction
 
 function! UltiSnips#ExpandSnippetOrJump() abort
     call s:compensate_for_pum()
-    exec g:_uspy "UltiSnips_Manager.expand_or_jump()"
+    py3 UltiSnips_Manager.expand_or_jump()
     return ""
 endfunction
 
 function! UltiSnips#ListSnippets() abort
-    exec g:_uspy "UltiSnips_Manager.list_snippets()"
+    py3 UltiSnips_Manager.list_snippets()
     return ""
 endfunction
 
@@ -90,49 +90,49 @@ function! UltiSnips#SnippetsInCurrentScope(...) abort
     if all
       let g:current_ulti_dict_info = {}
     endif
-    exec g:_uspy "UltiSnips_Manager.snippets_in_current_scope(" . all . ")"
+    py3 UltiSnips_Manager.snippets_in_current_scope(int(vim.eval("all")))
     return g:current_ulti_dict
 endfunction
 
 function! UltiSnips#SaveLastVisualSelection() range abort
-    exec g:_uspy "UltiSnips_Manager._save_last_visual_selection()"
+    py3 UltiSnips_Manager._save_last_visual_selection()
     return ""
 endfunction
 
 function! UltiSnips#JumpBackwards() abort
     call s:compensate_for_pum()
-    exec g:_uspy "UltiSnips_Manager.jump_backwards()"
+    py3 UltiSnips_Manager.jump_backwards()
     return ""
 endfunction
 
 function! UltiSnips#JumpForwards() abort
     call s:compensate_for_pum()
-    exec g:_uspy "UltiSnips_Manager.jump_forwards()"
+    py3 UltiSnips_Manager.jump_forwards()
     return ""
 endfunction
 
 function! UltiSnips#AddSnippetWithPriority(trigger, value, description, options, filetype, priority) abort
-    exec g:_uspy "trigger = vim.eval(\"a:trigger\")"
-    exec g:_uspy "value = vim.eval(\"a:value\")"
-    exec g:_uspy "description = vim.eval(\"a:description\")"
-    exec g:_uspy "options = vim.eval(\"a:options\")"
-    exec g:_uspy "filetype = vim.eval(\"a:filetype\")"
-    exec g:_uspy "priority = vim.eval(\"a:priority\")"
-    exec g:_uspy "UltiSnips_Manager.add_snippet(trigger, value, description, options, filetype, priority)"
+    py3 trigger = vim.eval("a:trigger")
+    py3 value = vim.eval("a:value")
+    py3 description = vim.eval("a:description")
+    py3 options = vim.eval("a:options")
+    py3 filetype = vim.eval("a:filetype")
+    py3 priority = vim.eval("a:priority")
+    py3 UltiSnips_Manager.add_snippet(trigger, value, description, options, filetype, priority)
     return ""
 endfunction
 
 function! UltiSnips#Anon(value, ...) abort
     " Takes the same arguments as SnippetManager.expand_anon:
     " (value, trigger="", description="", options="")
-    exec g:_uspy "args = vim.eval(\"a:000\")"
-    exec g:_uspy "value = vim.eval(\"a:value\")"
-    exec g:_uspy "UltiSnips_Manager.expand_anon(value, *args)"
+    py3 args = vim.eval("a:000")
+    py3 value = vim.eval("a:value")
+    py3 UltiSnips_Manager.expand_anon(value, *args)
     return ""
 endfunction
 
 function! UltiSnips#CursorMoved() abort
-    exec g:_uspy "UltiSnips_Manager._cursor_moved()"
+    py3 UltiSnips_Manager._cursor_moved()
 endf
 
 function! UltiSnips#LeavingBuffer() abort
@@ -140,19 +140,19 @@ function! UltiSnips#LeavingBuffer() abort
     let to_preview = getwinvar(winnr(), '&previewwindow')
 
     if !(from_preview || to_preview)
-        exec g:_uspy "UltiSnips_Manager._leaving_buffer()"
+        py3 UltiSnips_Manager._leaving_buffer()
     endif
 endf
 
 function! UltiSnips#LeavingInsertMode() abort
-    exec g:_uspy "UltiSnips_Manager._leaving_insert_mode()"
+    py3 UltiSnips_Manager._leaving_insert_mode()
 endfunction
 
 function! UltiSnips#TrackChange() abort
-    exec g:_uspy "UltiSnips_Manager._track_change()"
+    py3 UltiSnips_Manager._track_change()
 endfunction
 
 function! UltiSnips#RefreshSnippets() abort
-    exec g:_uspy "UltiSnips_Manager._refresh_snippets()"
+    py3 UltiSnips_Manager._refresh_snippets()
 endfunction
 " }}}

pythonx/UltiSnips/snippet_manager.py
@@ -7,13 +7,11 @@ from collections import defaultdict
 from contextlib import contextmanager
 import os
 import platform
-import sys
 import vim
 
 from UltiSnips import vim_helper
 from UltiSnips import err_to_scratch_buffer
 from UltiSnips.diff import diff, guess_edit
-from UltiSnips.compatibility import as_unicode
 from UltiSnips.position import Position
 from UltiSnips.snippet.definition import UltiSnipsSnippetDefinition
 from UltiSnips.snippet.source import (
@@ -51,8 +49,7 @@ def _ask_snippets(snippets):
     """Given a list of snippets, ask the user which one they want to use, and
     return it."""
     display = [
-        as_unicode("%i: %s (%s)")
-        % (i + 1, escape(s.description, "\\"), escape(s.location, "\\"))
+        "%i: %s (%s)" % (i + 1, escape(s.description, "\\"), escape(s.location, "\\"))
         for i, s in enumerate(snippets)
     ]
     return _ask_user(snippets, display)
@@ -60,7 +57,7 @@ def _ask_snippets(snippets):
 
 # TODO(sirver): This class is still too long. It should only contain public
 # facing methods, most of the private methods should be moved outside of it.
-class SnippetManager(object):
+class SnippetManager:
 
     """The main entry point for all UltiSnips functionality.
 
@@ -165,8 +162,7 @@ class SnippetManager(object):
 
             location = snip.location if snip.location else ""
 
-            key = as_unicode(snip.trigger)
-            description = as_unicode(description)
+            key = snip.trigger
 
             # remove surrounding "" or '' in snippet description if it exists
             if len(description) > 2:
@@ -174,20 +170,18 @@ class SnippetManager(object):
                     description = description[1:-1]
 
             vim_helper.command(
-                as_unicode("let g:current_ulti_dict['{key}'] = '{val}'").format(
+                "let g:current_ulti_dict['{key}'] = '{val}'".format(
                     key=key.replace("'", "''"), val=description.replace("'", "''")
                 )
             )
 
             if search_all:
                 vim_helper.command(
-                    as_unicode(
-                        (
-                            "let g:current_ulti_dict_info['{key}'] = {{"
-                            "'description': '{description}',"
-                            "'location': '{location}',"
-                            "}}"
-                        )
+                    (
+                        "let g:current_ulti_dict_info['{key}'] = {{"
+                        "'description': '{description}',"
+                        "'location': '{location}',"
+                        "}}"
                     ).format(
                         key=key.replace("'", "''"),
                         location=location.replace("'", "''"),
@@ -726,8 +720,8 @@ class SnippetManager(object):
             with use_proxy_buffer(self._active_snippets, self._vstate):
                 with self._action_context():
                     snippet.do_post_expand(
-                        snippet_instance._start,
-                        snippet_instance._end,
+                        snippet_instance.start,
+                        snippet_instance.end,
                         self._active_snippets,
                     )
 
@@ -850,8 +844,7 @@ class SnippetManager(object):
         if len(potentials) > 1:
             files = sorted(potentials)
             formatted = [
-                as_unicode("%i: %s") % (i, escape(fn, "\\"))
-                for i, fn in enumerate(files, 1)
+                "%i: %s" % (i, escape(fn, "\\")) for i, fn in enumerate(files, 1)
             ]
             file_to_edit = _ask_user(files, formatted)
             if file_to_edit is None:
@@ -882,16 +875,12 @@ class SnippetManager(object):
         self._should_update_textobjects = True
 
         try:
-            inserted_char = vim_helper.as_unicode(vim_helper.eval("v:char"))
+            inserted_char = vim_helper.eval("v:char")
         except UnicodeDecodeError:
             return
 
-        if sys.version_info >= (3, 0):
-            if isinstance(inserted_char, bytes):
-                return
-        else:
-            if not isinstance(inserted_char, unicode):
-                return
+        if isinstance(inserted_char, bytes):
+            return
 
         try:
             if inserted_char == "":

plugin/UltiSnips.vim
@@ -10,36 +10,6 @@ if version < 704
    finish
 endif
 
-if !exists("g:UltiSnipsUsePythonVersion")
-   let g:_uspy=":py3 "
-   if !has("python3")
-       if !has("python")
-           if !exists("g:UltiSnipsNoPythonWarning")
-               echohl WarningMsg
-               echom  "UltiSnips requires py >= 2.7 or py3"
-               echohl None
-           endif
-           unlet g:_uspy
-           finish
-       endif
-       let g:_uspy=":py "
-   endif
-else
-   " Use user-provided value, but check if it's available.
-   " This uses `has()`, because e.g. `exists(":python3")` is always 2.
-   if g:UltiSnipsUsePythonVersion == 2 && has('python')
-       let g:_uspy=":python "
-   elseif g:UltiSnipsUsePythonVersion == 3 && has('python3')
-       let g:_uspy=":python3 "
-   endif
-   if !exists('g:_uspy')
-       echohl WarningMsg
-       echom  "UltiSnips: the Python version from g:UltiSnipsUsePythonVersion (".g:UltiSnipsUsePythonVersion.") is not available."
-       echohl None
-       finish
-   endif
-endif
-
 " The Commands we define.
 command! -bang -nargs=? -complete=customlist,UltiSnips#FileTypeComplete UltiSnipsEdit
     \ :call UltiSnips#Edit(<q-bang>, <q-args>)

.gitignore
@@ -1,4 +1,5 @@
 *.pyc
 *.swp
 doc/tags
-.ropeproject/
+/.ropeproject/
+/.mypy_cache/

.travis.yml
@@ -2,10 +2,6 @@ sudo: required
 services: docker
 
 env:
-  - VIM_VERSION="7.4" PYTHON_IMAGE=2.7-stretch TAG=vim_74_py2
-  - VIM_VERSION="8.0" PYTHON_IMAGE=2.7-stretch TAG=vim_80_py2
-  - VIM_VERSION="8.1" PYTHON_IMAGE=2.7-stretch TAG=vim_81_py2
-  - VIM_VERSION="git" PYTHON_IMAGE=2.7-stretch TAG=vim_git_py2
   - VIM_VERSION="7.4" PYTHON_IMAGE=3.6-stretch TAG=vim_74_py36
   - VIM_VERSION="8.0" PYTHON_IMAGE=3.6-stretch TAG=vim_80_py36
   - VIM_VERSION="8.1" PYTHON_IMAGE=3.6-stretch TAG=vim_81_py36

Makefile
@@ -2,14 +2,6 @@ MAKEFILE_PATH := $(abspath $(lastword $(MAKEFILE_LIST)))
 MAKEFILE_DIR := $(dir ${MAKEFILE_PATH})
 
 # Test images as run on CI.
-image_vim_74_py2:
-	docker build -t ultisnips:$@ --build-arg PYTHON_IMAGE=2.7-stretch --build-arg VIM_VERSION=7.4 .
-image_vim_80_py2:
-	docker build -t ultisnips:$@ --build-arg PYTHON_IMAGE=2.7-stretch --build-arg VIM_VERSION=8.0 .
-image_vim_81_py2:
-	docker build -t ultisnips:$@ --build-arg PYTHON_IMAGE=2.7-stretch --build-arg VIM_VERSION=8.1 .
-image_vim_git_py2:
-	docker build -t ultisnips:$@ --build-arg PYTHON_IMAGE=2.7-stretch --build-arg VIM_VERSION=git .
 image_vim_74_py36:
 	docker build -t ultisnips:$@ --build-arg PYTHON_IMAGE=3.6-stretch --build-arg VIM_VERSION=7.4 .
 image_vim_80_py36:

after/plugin/UltiSnips_after.vim
@@ -1,6 +1,6 @@
 " Called after everything else to reclaim keys (Needed for Supertab)
 
-if exists("b:did_after_plugin_ultisnips_after") || !exists("g:_uspy")
+if exists("b:did_after_plugin_ultisnips_after")
    finish
 endif
 let b:did_after_plugin_ultisnips_after = 1

autoload/UltiSnips/map_keys.vim
@@ -1,4 +1,4 @@
-if exists("b:did_autoload_ultisnips_map_keys") || !exists("g:_uspy")
+if exists("b:did_autoload_ultisnips_map_keys")
    finish
 endif
 let b:did_autoload_ultisnips_map_keys = 1

commit f7b9b3b1100b3b2883ed2d0ac3627708a10dc469

Fixes :UltiSnipsAddFiletypes no longer working as advertised (#1140)
This was broken in #1129 through a search & replaco. 

Fixes #1139.

autoload/UltiSnips.vim
@@ -48,7 +48,7 @@ function! UltiSnips#Edit(bang, ...) abort
 endfunction
 
 function! UltiSnips#AddFiletypes(filetypes) abort
-    py3 UltiSnips_Manager.add_buffer_filetypes('" . a:filetypes . "')
+    py3 UltiSnips_Manager.add_buffer_filetypes(vim.eval("a:filetypes"))
     return ""
 endfunction
 

pythonx/UltiSnips/snippet_manager.py
@@ -321,7 +321,8 @@ class SnippetManager:
             + ["all"]
         )
 
-    def add_buffer_filetypes(self, filetypes):
+    def add_buffer_filetypes(self, filetypes: str):
+        """'filetypes' is a dotted filetype list, for example 'cuda.cpp'"""
         buf_fts = self._added_buffer_filetypes[vim_helper.buf.number]
         idx = -1
         for ft in filetypes.split("."):