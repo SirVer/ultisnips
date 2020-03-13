199aefd1b04958f2ca2025549d594c315a85011f
Morten Linderud <morten@linderud.pw>
2020-03-13 23:22:28 +0100

    [vim_helper] Properly support XDG_CONFIG_HOME for vim
    Currently we only care about XDG_CONFIG_HOME if nvim is installed. But
    vim can use this directory as well, so we explicitly want to have it on
    our discovery path.
    
    Signed-off-by: Morten Linderud <morten@linderud.pw>

--- pythonx/UltiSnips/vim_helper.py
+++ pythonx/UltiSnips/vim_helper.py
@@ -218,9 +218,14 @@ def get_dot_vim():
     candidates = []
     if platform.system() == "Windows":
         candidates.append(os.path.join(home, "vimfiles"))
+
     if vim.eval("has('nvim')") == "1":
         xdg_home_config = vim.eval("$XDG_CONFIG_HOME") or os.path.join(home, ".config")
         candidates.append(os.path.join(xdg_home_config, "nvim"))
+
+    if vim.eval("$XDG_CONFIG_HOME"):
+        xdg_home_config = vim.eval("$XDG_CONFIG_HOME") or os.path.join(home, ".config")
+        candidates.append(os.path.join(xdg_home_config, "vim"))
 
     candidates.append(os.path.join(home, ".vim"))
 
