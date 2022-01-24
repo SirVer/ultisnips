f160a9140b2c287cdb6ce390c9f8362ee453b5de
Holger Rapp <HolgerRapp@gmx.net>
2022-01-24 14:27:35 +0100

    Increase verbosity of test runs on CI
    This is a crude attempt to track and shut down flaky test runs.

--- docker/run_tests.sh
+++ docker/run_tests.sh
@@ -13,11 +13,21 @@ set -x
 
 tmux new -d -s vim
 
-$PYTHON_CMD ./test_all.py \
+# Tests on CI sometimes hang, but we do not know which test it is for sure,
+# since all we see are lines of the prior passed tests. In an attempt to have
+# every character appear unbuffered we hope to uncover where the test actually
+# hangs.
+# We also use the `ts` tool to inform us when each line was printed to increase
+# the likelyhood to find the failing test faster. Adding these debug helps seem
+# to have reduced the likelyhood of the tests failing though.
+# See https://stackoverflow.com/questions/3465619/how-to-make-output-of-any-shell-command-unbuffered/25548995
+stdbuf -i0 -o0 -e0 \
+   $PYTHON_CMD ./test_all.py \
    -v \
    --failfast \
    --plugins \
    --session vim \
    --vim $VIM \
    --interface tmux \
-   --expected-python-version $PYTHON_VERSION
+   --expected-python-version $PYTHON_VERSION \
+   2>&1 | ts '[%Y-%m-%d %H:%M:%S]'
