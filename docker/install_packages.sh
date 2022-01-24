ccc7d16dcff22c38cbcfc62676a6064465247e1e
Holger Rapp <HolgerRapp@gmx.net>
2022-01-24 09:59:47 +0100

    More output.

--- docker/install_packages.sh
+++ docker/install_packages.sh
@@ -6,6 +6,7 @@ set -o verbose
 apt-get update
 apt-get install -y \
     g++ \
+    moreutils \
     tmux \
     git
 apt-get clean
--- docker/run_tests.sh
+++ docker/run_tests.sh
@@ -26,4 +26,4 @@ stdbuf -i0 -o0 -e0 \
    --session vim \
    --vim $VIM \
    --interface tmux \
-   --expected-python-version $PYTHON_VERSION
+   --expected-python-version $PYTHON_VERSION | ts '[%Y-%m-%d %H:%M:%S]'
