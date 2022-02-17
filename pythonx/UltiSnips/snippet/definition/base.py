3f8bdedd574633979f71822325b1da91762935f9
Carlos Esparza <ces42@users.noreply.github.com>
2022-02-17 05:27:01 -0800

    Expose text in line before snippet to context conditions (#1432)
    * expose text in line before snippet to context conditions
    
    * add documentation for snip.before
    
    * add test for snip.before

--- pythonx/UltiSnips/snippet/definition/base.py
+++ pythonx/UltiSnips/snippet/definition/base.py
@@ -137,7 +137,7 @@ class SnippetDefinition:
             return match
         return False
 
-    def _context_match(self, visual_content):
+    def _context_match(self, visual_content, before):
         # skip on empty buffer
         if len(vim.current.buffer) == 1 and vim.current.buffer[0] == "":
             return
@@ -147,6 +147,7 @@ class SnippetDefinition:
             "visual_mode": "",
             "visual_text": "",
             "last_placeholder": None,
+            "before": before,
         }
 
         if visual_content:
@@ -330,7 +331,7 @@ class SnippetDefinition:
 
         self._context = None
         if match and self._context_code:
-            self._context = self._context_match(visual_content)
+            self._context = self._context_match(visual_content, before)
             if not self.context:
                 match = False
 
