51053d6e56b9b4c69ec0bda9e472810c8d390dfa
David Briscoe <idbrii@gmail.com>
2025-01-07 21:39:04 -0800

    Switch from ftp.vim to ftp.nluug
    Fix "curl: (6) Could not resolve host: ftp.vim.org" when running tests.
    
    This is the link on https://www.vim.org/download.php#unix on this line:
    > There is one big file to download that contains almost everything. It
    > is found in the unix directory
    
    Looks like that change happened years ago to use https instead of ftp:
    https://github.com/vim/vim/issues/3580

--- docker/download_vim.sh
+++ docker/download_vim.sh
@@ -8,7 +8,7 @@ mkdir -p /src && cd /src
 if [[ $VIM_VERSION == "git" ]]; then
    git clone https://github.com/vim/vim
 else
-   curl http://ftp.vim.org/pub/vim/unix/vim-${VIM_VERSION}.tar.bz2 -o vim.tar.bz2
+   curl https://ftp.nluug.nl/pub/vim/unix/vim-${VIM_VERSION}.tar.bz2 -o vim.tar.bz2
    tar xjf vim.tar.bz2
    mv -v vim?? vim
 fi
