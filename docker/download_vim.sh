057faa3b2861874295fbfcafc14ca7f536b53a50
Holger Rapp <HolgerRapp@gmx.net>
2025-03-09 20:09:58 +0100

    Refix downloading.

--- docker/download_vim.sh
+++ docker/download_vim.sh
@@ -8,7 +8,7 @@ mkdir -p /src && cd /src
 if [[ $VIM_VERSION == "git" ]]; then
    git clone https://github.com/vim/vim
 else
-   curl https://github.com/vim/vim/archive/refs/tags/v${VIM_VERSION}.0.tar.gz  -o vim.tar.gz
-   tar xf vim.tar.bz2
-   mv -v vim?? vim
+   curl -L https://github.com/vim/vim/archive/refs/tags/v${VIM_VERSION}.0000.tar.gz  -o vim.tar.gz
+   tar xf vim.tar.gz && rm vim.tar.gz
+   mv -v vim* vim
 fi
