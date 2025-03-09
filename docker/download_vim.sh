f267413b63ec4b29946a17d446157e36857723a2
Holger Rapp <HolgerRapp@gmx.net>
2025-03-09 20:04:40 +0100

    Download from github.

--- docker/download_vim.sh
+++ docker/download_vim.sh
@@ -8,7 +8,7 @@ mkdir -p /src && cd /src
 if [[ $VIM_VERSION == "git" ]]; then
    git clone https://github.com/vim/vim
 else
-   curl https://ftp.nluug.nl/pub/vim/unix/vim-${VIM_VERSION}.tar.bz2 -o vim.tar.bz2
-   tar xjf vim.tar.bz2
+   curl https://github.com/vim/vim/archive/refs/tags/v${VIM_VERSION}.0.tar.gz  -o vim.tar.gz
+   tar xf vim.tar.bz2
    mv -v vim?? vim
 fi
