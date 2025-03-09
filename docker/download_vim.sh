557824c9749757f30b40640e00d029abc9b04de8
Holger Rapp <HolgerRapp@gmx.net>
2025-03-09 20:21:32 +0100

    Exclude py13 before 9.1.0417, since it does not work.

--- docker/download_vim.sh
+++ docker/download_vim.sh
@@ -8,7 +8,7 @@ mkdir -p /src && cd /src
 if [[ $VIM_VERSION == "git" ]]; then
    git clone https://github.com/vim/vim
 else
-   curl -L https://github.com/vim/vim/archive/refs/tags/v${VIM_VERSION}.0000.tar.gz  -o vim.tar.gz
+   curl -L https://github.com/vim/vim/archive/refs/tags/v${VIM_VERSION}.tar.gz  -o vim.tar.gz
    tar xf vim.tar.gz && rm vim.tar.gz
    mv -v vim* vim
 fi
