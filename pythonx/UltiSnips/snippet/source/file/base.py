1a0bb5d3c01870b6e1092ca1a6a23f629d99713c
Michael Henry <drmikehenry@drmikehenry.com>
2026-03-24 05:00:58 -0400

    Revert "fix #1165: insert mode lag (#1552)"
    This reverts commit 49dc8cb2086db19d385791c8e1635723b0fd95c7.
    
    Pull request #1552 (fixing issue #1165) introduced a caching bug that can
    prevent snippets from loading for a given filetype.
    
    To reproduce the problem:
    
    - Create a fake home directory for clean testing:
    
          mkdir fakehome
          cd fakehome
    
    - Clone the `ultisnips` repository as a fake `.vim` area so that
      `ultisnips` is automatically found on Vim's `runtimepath`:
    
          git clone https://github.com/SirVer/ultisnips .vim
    
    - Checkout commit 34d3ef028f19d34106b4eeadeb6c11b249e26887 (the current
      HEAD) for reproducibility:
    
          git -C .vim checkout 34d3ef028f19d34106b4eeadeb6c11b249e26887
    
    - Create `func` snippets for both C and Python:
    
          mkdir .vim/UltiSnips
    
          cat <<'EOF' > .vim/UltiSnips/c.snippets
          snippet func
          void func(void)
          {
          }
          endsnippet
          EOF
    
          cat <<'EOF' > .vim/UltiSnips/python.snippets
          snippet func
          def func():
              pass
          endsnippet
          EOF
    
    - Launch Vim with `HOME` set to the fake home directory:
    
          HOME=$PWD vim
    
    - Do something to autoload `ultisnips` to install the autocmds from pull
      request #1552.  The easiest thing is to insert some text into any
      buffer:
    
          iHello<Esc>
    
    - Edit a `.c` file but don't insert any snippets yet:
    
          :e! file.c
    
    - Edit a Python file and insert a `func` snippet:
    
          :e file.py
          ifunc<Tab><Esc>
    
      The snippet expands correctly.
    
    - Edit a C file and attempt to insert a `func` snippet:
    
          :e! file.c
          ifunc<Tab><Esc>
    
      The snippet does not expand.

--- pythonx/UltiSnips/snippet/source/file/base.py
+++ pythonx/UltiSnips/snippet/source/file/base.py
@@ -27,11 +27,9 @@ class SnippetFileSource(SnippetSource):
         SnippetSource.__init__(self)
 
     def ensure(self, filetypes):
-        if self.must_ensure:
-            for ft in self.get_deep_extends(filetypes):
-                if self._needs_update(ft):
-                    self._load_snippets_for(ft)
-            self.must_ensure = False
+        for ft in self.get_deep_extends(filetypes):
+            if self._needs_update(ft):
+                self._load_snippets_for(ft)
 
     def refresh(self):
         self.__init__()
