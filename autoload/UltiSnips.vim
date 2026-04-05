SirVer/ultisnips

commit 49dc8cb2086db19d385791c8e1635723b0fd95c7

fix #1165: insert mode lag (#1552)
* fix #1165: insert mode lag

Ultisnips was traversing the whole runtimepath every time an expansion
was attempted, something that happens for every key press since there is
autotrigger.

Even without autotrigger, traversal was done at every snippet expansion,
which caused slowdowns in that case.

With this change, traversal happens only the first time, or after
a source must be refreshed (because a snippets file has been updated).

autoload/UltiSnips.vim
@@ -3,6 +3,9 @@ if exists("b:did_autoload_ultisnips")
 endif
 let b:did_autoload_ultisnips = 1
 
+" Ensure snippets are loaded for current buffer
+au UltiSnips_AutoTrigger FileType,BufEnter * call UltiSnips#CheckFiletype()
+
 " Also import vim as we expect it to be imported in many places.
 py3 import vim
 py3 from UltiSnips import UltiSnips_Manager
@@ -186,6 +189,10 @@ endfunction
 
 function! UltiSnips#TrackChange() abort
     py3 UltiSnips_Manager._track_change()
+endfunction
+
+function! UltiSnips#CheckFiletype() abort
+    py3 UltiSnips_Manager._check_filetype(vim.eval('&ft'))
 endfunction
 
 function! UltiSnips#RefreshSnippets() abort

pythonx/UltiSnips/snippet/source/base.py
@@ -15,6 +15,7 @@ class SnippetSource:
     def __init__(self):
         self._snippets = defaultdict(SnippetDictionary)
         self._extends = defaultdict(set)
+        self.must_ensure = True
 
     def ensure(self, filetypes):
         """Ensures that snippets are loaded."""

pythonx/UltiSnips/snippet/source/file/base.py
@@ -27,9 +27,11 @@ class SnippetFileSource(SnippetSource):
         SnippetSource.__init__(self)
 
     def ensure(self, filetypes):
-        for ft in self.get_deep_extends(filetypes):
-            if self._needs_update(ft):
-                self._load_snippets_for(ft)
+        if self.must_ensure:
+            for ft in self.get_deep_extends(filetypes):
+                if self._needs_update(ft):
+                    self._load_snippets_for(ft)
+            self.must_ensure = False
 
     def refresh(self):
         self.__init__()

pythonx/UltiSnips/snippet_manager.py
@@ -130,6 +130,7 @@ class SnippetManager:
         self._visual_content = VisualContentPreserver()
 
         self._snippet_sources = []
+        self._filetypes = []
 
         self._snip_expanded_in_action = False
         self._inside_action = False
@@ -982,6 +983,15 @@ class SnippetManager:
     def _refresh_snippets(self):
         for _, source in self._snippet_sources:
             source.refresh()
+
+
+    @err_to_scratch_buffer.wrap
+    def _check_filetype(self, ft):
+        """Ensure snippets are loaded for the current filetype."""
+        if ft not in self._filetypes:
+            self._filetypes.append(ft)
+            for _, source in self._snippet_sources:
+                source.must_ensure = True
 
 
 UltiSnips_Manager = SnippetManager(  # pylint:disable=invalid-name

commit 191be4fc6bbf73f50de7d4144986c00d20123437

Fix snippet-loading failure caused by caching bug from pull request #1552 (#1585)
* Revert "fix #1165: insert mode lag (#1552)"

This reverts commit 49dc8cb2086db19d385791c8e1635723b0fd95c7.

Pull request #1552 (fixing issue #1165) introduced a caching bug that can
prevent snippets from loading for a given filetype.

To reproduce the problem:

- Create a fake home directory for clean testing:

      mkdir fakehome
      cd fakehome

- Clone the `ultisnips` repository as a fake `.vim` area so that
  `ultisnips` is automatically found on Vim's `runtimepath`:

      git clone https://github.com/SirVer/ultisnips .vim

- Checkout commit 34d3ef028f19d34106b4eeadeb6c11b249e26887 (the current
  HEAD) for reproducibility:

      git -C .vim checkout 34d3ef028f19d34106b4eeadeb6c11b249e26887

- Create `func` snippets for both C and Python:

      mkdir .vim/UltiSnips

      cat <<'EOF' > .vim/UltiSnips/c.snippets
      snippet func
      void func(void)
      {
      }
      endsnippet
      EOF

      cat <<'EOF' > .vim/UltiSnips/python.snippets
      snippet func
      def func():
          pass
      endsnippet
      EOF

- Launch Vim with `HOME` set to the fake home directory:

      HOME=$PWD vim

- Do something to autoload `ultisnips` to install the autocmds from pull
  request #1552.  The easiest thing is to insert some text into any
  buffer:

      iHello<Esc>

- Edit a `.c` file but don't insert any snippets yet:

      :e! file.c

- Edit a Python file and insert a `func` snippet:

      :e file.py
      ifunc<Tab><Esc>

  The snippet expands correctly.

- Edit a C file and attempt to insert a `func` snippet:

      :e! file.c
      ifunc<Tab><Esc>

  The snippet does not expand.

* Fix insert mode lag (#1552)

Ensure that snippet-loading is attempted for a filetype only once, even
if no snippets are defined for that filetype.  Do this by ensuring that
`self._snippets[ft]` will exist unconditionally for each filetype `ft`
for which snippet loading is attempted.

autoload/UltiSnips.vim
@@ -3,9 +3,6 @@ if exists("b:did_autoload_ultisnips")
 endif
 let b:did_autoload_ultisnips = 1
 
-" Ensure snippets are loaded for current buffer
-au UltiSnips_AutoTrigger FileType,BufEnter * call UltiSnips#CheckFiletype()
-
 " Also import vim as we expect it to be imported in many places.
 py3 import vim
 py3 from UltiSnips import UltiSnips_Manager
@@ -189,10 +186,6 @@ endfunction
 
 function! UltiSnips#TrackChange() abort
     py3 UltiSnips_Manager._track_change()
-endfunction
-
-function! UltiSnips#CheckFiletype() abort
-    py3 UltiSnips_Manager._check_filetype(vim.eval('&ft'))
 endfunction
 
 function! UltiSnips#RefreshSnippets() abort

pythonx/UltiSnips/snippet/source/base.py
@@ -15,7 +15,6 @@ class SnippetSource:
     def __init__(self):
         self._snippets = defaultdict(SnippetDictionary)
         self._extends = defaultdict(set)
-        self.must_ensure = True
 
     def ensure(self, filetypes):
         """Ensures that snippets are loaded."""

pythonx/UltiSnips/snippet/source/file/base.py
@@ -27,11 +27,9 @@ class SnippetFileSource(SnippetSource):
         SnippetSource.__init__(self)
 
     def ensure(self, filetypes):
-        if self.must_ensure:
-            for ft in self.get_deep_extends(filetypes):
-                if self._needs_update(ft):
-                    self._load_snippets_for(ft)
-            self.must_ensure = False
+        for ft in self.get_deep_extends(filetypes):
+            if self._needs_update(ft):
+                self._load_snippets_for(ft)
 
     def refresh(self):
         self.__init__()
@@ -58,6 +56,10 @@ class SnippetFileSource(SnippetSource):
         for parent_ft in self.get_deep_extends([ft]):
             if parent_ft != ft and self._needs_update(parent_ft):
                 self._load_snippets_for(parent_ft)
+        # Make sure the dictionary will exist even if no snippets are found;
+        # this ensures each `ft` is scanned only once, preventing expensive
+        # searches down Vim's 'runtimepath'.
+        self._snippets[ft]
 
     def _parse_snippets(self, ft, filename):
         """Parse the 'filename' for the given 'ft'."""

pythonx/UltiSnips/snippet_manager.py
@@ -130,7 +130,6 @@ class SnippetManager:
         self._visual_content = VisualContentPreserver()
 
         self._snippet_sources = []
-        self._filetypes = []
 
         self._snip_expanded_in_action = False
         self._inside_action = False
@@ -983,15 +982,6 @@ class SnippetManager:
     def _refresh_snippets(self):
         for _, source in self._snippet_sources:
             source.refresh()
-
-
-    @err_to_scratch_buffer.wrap
-    def _check_filetype(self, ft):
-        """Ensure snippets are loaded for the current filetype."""
-        if ft not in self._filetypes:
-            self._filetypes.append(ft)
-            for _, source in self._snippet_sources:
-                source.must_ensure = True
 
 
 UltiSnips_Manager = SnippetManager(  # pylint:disable=invalid-name